028322e608a092d2e91e0067d583a9c16b472544
Jose Condori <josericardo3396@gmail.com>
2022-01-26 01:23:27 -0300

    se agrego la ruta correspondiente a la operacion de obtener todos los usuarios

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,16 +1,12 @@
 import { Router } from "express";
-import {
-  registerUser,
-  authUser,
-  getUserProfile,
-  updateUserProfile,
-} from "../controllers/userController.js";
-import { protect } from "../middlewares/authMiddleware.js";
+import * as userCtrl from "../controllers/userController.js";
+import { protect, admin } from "../middlewares/authMiddleware.js";
 const router = Router();
 
-router.post("/", registerUser);
-router.post("/login", authUser);
-router.get("/profile", protect, getUserProfile);
-router.put("/profile", protect, updateUserProfile);
+router.post("/", userCtrl.registerUser);
+router.post("/login", userCtrl.authUser);
+router.get("/profile", protect, userCtrl.getUserProfile);
+router.put("/profile", protect, userCtrl.updateUserProfile);
+router.get("/all-users", protect, admin, userCtrl.getUsers);
 
 export default router;
