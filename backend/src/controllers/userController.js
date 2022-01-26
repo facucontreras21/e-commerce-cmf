520cd70f52fcdc8120598e14e558e71b8da27fc7
Jose Condori <josericardo3396@gmail.com>
2022-01-26 10:02:23 -0300

    se agregaron los estados de las respuestas en la solicitudes

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -98,14 +98,14 @@ export const updateUserProfile = asyncHandler(async (req, res) => {
 
 export const getUsers = asyncHandler(async (req, res) => {
   const users = await User.find().select("-password");
-  res.json(users);
+  res.status(200).json(users);
 });
 
 export const deleteUser = asyncHandler(async (req, res) => {
   const userExists = await User.findById(req.params.id);
   if (userExists) {
     const result = await User.remove({ _id: userExists._id }); //duda - consultar
-    res.json({ message: "User removed" });
+    res.status(200).json({ message: "User removed" });
   } else {
     res.status(404).json({ message: "User not found" });
   }
@@ -114,7 +114,7 @@ export const deleteUser = asyncHandler(async (req, res) => {
 export const getUserById = asyncHandler(async (req, res) => {
   const userExists = await User.findById(req.params.id).select("-password");
   if (userExists)
-    res.json({
+    res.status(200).json({
       _id: userExists._id,
       name: userExists.name,
       email: userExists.email,
@@ -136,7 +136,7 @@ export const updateUser = asyncHandler(async (req, res) => {
 
     userExists.save();
 
-    res.json({
+    res.status(200).json({
       _id: userExists._id,
       name: userExists.name,
       email: userExists.email,
