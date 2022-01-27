0197843aabd59b2b63dd7cd72f02e836e7ebc761
Jose Condori <josericardo3396@gmail.com>
2022-01-27 09:34:30 -0300

    creacion del archivo orderRoutes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import userRouter from "./userRoutes.js";
 import productRouter from "./productRoutes.js";
+import orderRouter from "./orderRoutes.js";
 
 const router = Router();
 
 router.use("/users", userRouter);
 router.use("/products", productRouter);
+router.use("/ordes", orderRouter);
 
 export default router;
