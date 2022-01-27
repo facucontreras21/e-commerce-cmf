d1c1b67bf23ee5418702929f1c66519194aaa0cb
facucontreras21 <facucontreras21@gmail.com>
2022-01-27 12:45:07 -0300

    controladores ordenes

--- backend/src/controllers/orderController.js
+++ backend/src/controllers/orderController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler";
 import Order from "../models/orderModel.js";
+import User from "../models/userModel.js";
 
 //@desc Add new order
 //@route POST /api/orders
@@ -35,20 +36,71 @@ export const addOrderItems = asyncHandler(async (req, res) => {
 //@desc Get order by ID
 //@route GET api/orders/:id
 //@access Private
-export const getOrderById = asyncHandler(async (req, res) => {});
+export const getOrderById = asyncHandler(async (req, res) => {
+  const orderExists = await Order.findById(req.params.id);
+  if (orderExists)
+    res.status(200).json({
+      name: req.user.name,
+      email: req.user.email,
+      orderExists,
+    });
+  else res.status(404).json({ message: "Order not found" });
+});
 //@desc Update order to paid
 //@route PUT api/orders/:id/pay
 //@access Private
-export const updateOrderToPaid = asyncHandler(async (req, res) => {});
+export const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const { id, status, update_time, email_address } = req.body;
+  const orderExists = await Order.findById(req.params.id);
+  if (orderExists) {
+    orderExists.isPaid = true;
+    orderExists.paidAt = Date.now();
+    orderExists.paymentResult = {
+      id,
+      status,
+      update_time,
+      email_address,
+    };
+    await orderExists.save();
+    res.status(200).json({ orderExists });
+  } else res.status(404).json({ message: "Order not found" });
+});
 //@desc Update order to delivered
 //@route PUT api/orders/:id/deliver
 //@access Private/Admin
-export const updateOrderToDelivered = asyncHandler(async (req, res) => {});
+export const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  const orderExists = await Order.findById(req.params.id);
+  if (orderExists) {
+    orderExists.isDelivered = true;
+    orderExists.deliveredAt = Date.now();
+    await orderExists.save();
+    res.status(200).json({ orderExists });
+  } else {
+    res.status(404).json({ message: "Order not found" });
+  }
+});
 //@desc Get logged in user orders
 //@route GET api/orders/myorders
 //@access Private
-export const getMyOrders = asyncHandler(async (req, res) => {});
-//@desc Get all orders
-//@route GET api/orders
-//@access Private/Admin
-export const getOrders = asyncHandler(async (req, res) => {});
+export const getMyOrders = asyncHandler(async (req, res) => {
+    const ordersOfUser = await Order.find({ user: req.user._id });
+    if (!ordersOfUser) {
+      res.status(404).json({ message: "No Orders" });
+    } else {
+      return res.status(200).json(ordersOfUser);
+    }
+  });
+  //@desc Get all orders
+  //@route GET api/orders
+  //@access Private/Admin
+  export const getOrders = asyncHandler(async (req, res) => {
+    const orders = await Order.find().populate({
+      path: "user",
+      select: "id name",
+    });
+    if (!orders) {
+      res.status(404).json({ message: "No Orders" });
+    } else {
+      return res.status(200).json(orders);
+    }
+  });
--- backend/src/routes/orderRoutes.js
+++ backend/src/routes/orderRoutes.js
@@ -4,5 +4,10 @@ import { protect, admin } from "../middlewares/authMiddleware.js";
 const router = Router();
 
 router.post("/", protect, orderCtrl.addOrderItems);
+router.get("/myorders", protect, orderCtrl.getMyOrders);
+router.get("/", protect, admin, orderCtrl.getOrders);
+router.get("/:id", protect, orderCtrl.getOrderById);
+router.put("/:id/pay", protect, orderCtrl.updateOrderToPaid);
+router.put("/:id/deliver", protect, orderCtrl.updateOrderToDelivered);
 
 export default router;
