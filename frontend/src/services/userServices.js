c8ab5bf60b7f687db39236196adf7c13e4b2eeba
facucontreras21 <facucontreras21@gmail.com>
2022-02-02 10:29:17 -0300

    userServices completo

--- frontend/src/services/userServices.js
+++ frontend/src/services/userServices.js
@@ -9,3 +9,127 @@ export const loginUsers = async (body) => {
     throw error;
   }
 };
+
+export const registerUser = async (name, email, password) => {
+  try {
+    const body = {
+      name,
+      email,
+      password,
+    };
+    const { data } = await axios.post(`${BASE_URL_BACK}/users/`, body);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const allUsers = async (token, isAdmin) => {
+  try {
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    const body = {
+      isAdmin,
+    };
+    const { data } = await axios.get(
+      `${BASE_URL_BACK}/users/all-users`,
+      body,
+      config
+    );
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const userProfile = async (token) => {
+  try {
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    const { data } = await axios.get(
+      `${BASE_URL_BACK}/users/profile`,
+      {},
+      config
+    );
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const modifyUserProfile = async (token, name, email, password) => {
+  try {
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    const body = {
+      name,
+      email,
+      password,
+    };
+    const { data } = await axios.put(
+      `${BASE_URL_BACK}/users/profile`,
+      body,
+      config
+    );
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteUserProfile = async (token) => {
+  try {
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    const { data } = await axios.delete(
+      `${BASE_URL_BACK}/users/:id`,
+      {},
+      config
+    );
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getUserProfile = async (token) => {
+  try {
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    const { data } = await axios.get(
+      `${BASE_URL_BACK}/users/:id`,
+      {},
+      config
+    );
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const putUserProfile = async (token,name,email,password,isAdmin) => {
+  try {
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    const body = {
+      name,
+      email,
+      password,
+      isAdmin
+    };
+    const { data } = await axios.put(
+      `${BASE_URL_BACK}/users/:id`,
+      body,
+      config
+    );
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
